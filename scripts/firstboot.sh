39418c03695335f4a77d100cf56741e4a838fc04
Darren Young <darren@yhlsecurity.com>
2019-02-11 00:12:29 -0600

    update

--- scripts/firstboot.sh
+++ scripts/firstboot.sh
@@ -1,40 +1,69 @@
 #!/bin/bash
 
-
-sudo apt-get update
-sudo apt-get -y install uucp ntpdate
-sudo apt-get -y upgrade
-reboot
+typeset -f hfNetChk
 
 
-Set the hostname
-    MAC=`ifconfig eth0 | grep ether | awk '{print $2}' | awk -F: '{print $5$6}'`
-    echo "pi-$MAC" > /etc/hostname
-    cat /etc/hosts | sed -e "s/raspberrypi/pi-$MAC/g" > /tmp/hosts && mv /tmp/hosts /etc/hosts
 
-Set keyboard language
-    cat /etc/default/keyboard | sed -e 's/XKBLAYOUT="gb"/XKBLAYOUT="us"/g' > /tmp/keyboard && mv /tmp/keyboard /etc/default/keyboard
-    sudo dpkg-reconfigure keyboard-configuration --frontend=noninteractive
+#
+if [ -f /etc/redhat-release ]; then
+    echo "You are on RedHat/Centos, this script is for Raspbian"
+    exit 1
+else 
+    echo "NOT running on RedHat/CentOS, good"
+fi
 
-Set timezone
-    echo "America/Chicago" > /etc/timezone
-    rm -f /etc/localtime
-    dpkg-reconfigure -f noninteractive tzdata
-    ntpdate pool.ntp.org
+#
+DIST=`/usr/bin/lsb_release -i | awk '{print $3}'`
+if [ ! $DIST == "Raspbian" ]; then
+    echo "While you appear to be on a Deb dist, it's not Raspbian, sorry"
+    exit 1
+else 
+    echo "Running on Raspbian, good"
+fi
 
-Set wifi region
-    echo "country=US" >> /etc/wpa_supplicant/wpa_supplicant.conf
 
-set up log forwarding
-    echo "*.*   @@xlog01:514" > /etc/rsyslog.d/loghost.conf
 
-Create the sysadmin account
-    useradd -c "System Admin" \
-            -d /home/sysadmin \
-            -G dialout,sudo,users,netdev \
-            -u 2323 \
-            -s /bin/bash \
-            --create-home \
-            sysadmin
-    echo "sysadmin ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/010_sysadmin-nopasswd
 
+
+
+
+
+# function to check if eth0 is on the correct internal subnet
+# this means DHCP worked and we're off the 169.254 net
+hfNetChk () {
+    echo "hfChkNet: checking network"
+    ifconfig eth0 | grep inet | grep -v inet6 | awk '{print $2}'
+    IP=`ifconfig eth0 | grep inet | grep -v inet6 | awk '{print $2}' | grep -q -F "192.168"`
+    ret=$?
+    return $ret
+}
+
+# check for successful DHCP on the 192.168 net
+# if so move on, else try again up to some number of times
+x=0
+maxNetRetries=60
+netChkSleepTime=1
+
+while ! hfNetChk
+do
+    if [ "$x" -ge $maxNetRetries ]; then
+        echo "timed out"
+        exit 47
+    else
+        x=$((x+1))
+        echo "Retry #: $x"
+        echo "Sleeping for network for $netChkSleepTime seconds"
+        sleep $netChkSleepTime
+    fi
+done
+
+echo "Got an IP from DHCP after $x seconds, moving on"
+echo "Getting stage 2 setup"
+
+if [ -x "/usr/bin/wget" ]; then
+    wget http://www.google.com > /google.out
+else
+    echo "wget not found, exiting"
+fi
+
+exit 0
