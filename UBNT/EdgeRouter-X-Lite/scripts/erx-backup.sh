31154d3af13789b93784d384c710fc6188b81832
root <root@erx01>
2019-02-25 04:39:22 -0600

    update

---
+++ UBNT/EdgeRouter-X-Lite/scripts/erx-backup.sh
@@ -0,0 +1,28 @@
+#!/bin/vbash
+#############################################################################
+#
+# erx-backup.sh
+#
+# Backs up an EdgeRouter-X to tftp server(s)
+#
+#############################################################################
+#
+# USAGE:
+#
+#   * Must have a working tftp server availble to the ER-X, testing was 
+#     done against a Debian machine running tftpd-hpa.
+#
+#############################################################################
+
+
+#############################################################################
+# VARIABLES
+#############################################################################
+DATE=`date +%Y-%m-%d-%H%M%S`
+
+#############################################################################
+# MAIN
+#############################################################################
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper save tftp://dns01/erx.config.$DATE
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper save tftp://dns02/erx.config.$DATE
